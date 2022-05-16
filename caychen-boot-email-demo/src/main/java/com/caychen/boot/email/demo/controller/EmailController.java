7db77365484ed2b7d1e2bd4c0c2ecc4232f71cb0
caychen <412425870@qq.com>
2022-05-16 13:18:39 +0800

    发送附件邮件

--- caychen-boot-email-demo/src/main/java/com/caychen/boot/email/demo/controller/EmailController.java
+++ caychen-boot-email-demo/src/main/java/com/caychen/boot/email/demo/controller/EmailController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.mail.MessagingException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Author: Caychen
  * @Date: 2022/5/9 21:13
@@ -24,6 +28,16 @@ public class EmailController {
     @GetMapping("/sendSimpleEmail")
     public R sendSimple(String subject, String text) {
         mailComponent.sendSimpleMail(subject, text);
+
+        return R.ok();
+    }
+
+    @GetMapping("/sendAttachmentEmail")
+    public R sendAttachmentEmail(String subject, String text) throws MessagingException {
+        Map<String, String> attachmentMap = new HashMap<>();
+        attachmentMap.put("附件.xlsx", "附件.xlsx的绝对路径");
+
+        mailComponent.sendHtmlMail(subject, text, attachmentMap);
 
         return R.ok();
     }
